863c6053d9c31d39b2a54ef6be809974dcf8f109
aamisubha <sbhadipchanda@gmail.com>
2025-09-24 14:06:01 +0530

    Corporate files order change

--- src/app/lib/getCorporateFile.js
+++ src/app/lib/getCorporateFile.js
@@ -1,6 +1,8 @@
 const corporateFile = {
     getAll: async ({ langLoc, type }) => {
-        const url = process.env.NEXT_PUBLIC_CMS_API_URL + `/corporate-files?populate=*&filters[locations][id][$eq]=${langLoc.loc.id}&filters[type][$eq]=${type}`;
+        const url = process.env.NEXT_PUBLIC_CMS_API_URL + `/corporate-files?populate=*&filters[locations][id][$eq]=${langLoc.loc.id}&filters[type][$eq]=${type}&sort=title:asc`;
+
+        console.log(url);
         const req = await fetch(url);
         const res = await req.json();
 
